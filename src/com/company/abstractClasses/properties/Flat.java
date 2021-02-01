9a54ab900972d901e4071416c1284de6bfc7168f
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-02-01 16:36:25 +0000

    add property exercise

--- src/com/company/abstractClasses/properties/Flat.java
+++ src/com/company/abstractClasses/properties/Flat.java
@@ -1,2 +1,9 @@
-package com.company.abstractClasses.properties;public class Flat {
+package com.company.abstractClasses.properties;
+
+public class Flat extends Property{
+    private Integer nthFloor;
+    public Flat(String location, Integer value, Integer rooms, Integer floor){
+        super(location,value,rooms);
+        this.nthFloor = floor;
+    }
 }
