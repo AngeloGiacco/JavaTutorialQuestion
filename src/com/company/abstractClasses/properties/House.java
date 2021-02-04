a13482d4e5a44d02cadcff881fd632c4bb9c9de4
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-02-04 16:36:37 +0000

    add property exercise

--- src/com/company/abstractClasses/properties/House.java
+++ src/com/company/abstractClasses/properties/House.java
@@ -1,2 +1,15 @@
-package com.company.abstractClasses.properties;public class House {
+package com.company.abstractClasses.properties;
+
+public class House extends Property {
+    private final Integer stories;
+    private final Type houseType;
+    public House(String location, Integer rooms, Integer value, Integer stories, Type houseType) {
+        super(location, rooms, value);
+        this.stories = stories;
+        this.houseType = houseType;
+    }
+
+    Type getHouseType() {
+        return houseType;
+    }
 }
