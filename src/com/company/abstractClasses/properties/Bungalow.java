15171409a6466f10cad139d61155277d0bcfd9e8
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-02-06 16:36:13 +0000

    add property exercise

---
+++ src/com/company/abstractClasses/properties/Bungalow.java
@@ -0,0 +1,7 @@
+package com.company.abstractClasses.properties;
+
+public class Bungalow extends House {
+    public Bungalow(String location, Integer rooms, Integer value, Type houseType) {
+        super(location, rooms, value, 1, houseType);
+    }
+}
