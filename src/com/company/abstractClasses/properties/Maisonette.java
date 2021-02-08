16abb5b8ae9edd27fe968703de877f88c6c81a2f
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-02-08 16:36:50 +0000

    add property exercise

--- src/com/company/abstractClasses/properties/Maisonette.java
+++ src/com/company/abstractClasses/properties/Maisonette.java
@@ -1,2 +1,9 @@
-package com.company.abstractClasses.properties;public class Maisonette {
+package com.company.abstractClasses.properties;
+
+public class Maisonette extends Flat{
+    private final Integer floors;
+    public Maisonette(String location, Integer value, Integer rooms, Integer floor, Integer floors){
+        super(location, value, rooms, floor);
+        this.floors = floors;
+    }
 }
