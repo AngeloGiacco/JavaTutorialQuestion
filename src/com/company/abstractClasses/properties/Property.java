ad77efe4efab96174afea21da95fcc9a6967a4a0
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-02-09 16:37:01 +0000

    add property exercise

--- src/com/company/abstractClasses/properties/Property.java
+++ src/com/company/abstractClasses/properties/Property.java
@@ -1,2 +1,21 @@
-package com.company.abstractClasses.properties;public class Property {
+package com.company.abstractClasses.properties;
+
+abstract class Property {
+    Integer value;
+    Integer rooms;
+    final String location;
+
+    public Property(String location, Integer value, Integer rooms) {
+        this.location = location;
+        this.value = value;
+        this.rooms = rooms;
+    }
+
+    public void increaseValue(Integer increase) {
+        this.value += increase;
+    }
+
+    public void decreaseValue(Integer decrease) {
+        this.increaseValue(-decrease);
+    }
 }
