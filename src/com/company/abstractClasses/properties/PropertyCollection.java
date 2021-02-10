fae64e0d5f504573699151256071db5f2dd20d7f
Angelo Giacco <giacco.angelo.s@gmail.com>
2021-02-10 16:37:08 +0000

    add property exercise

--- src/com/company/abstractClasses/properties/PropertyCollection.java
+++ src/com/company/abstractClasses/properties/PropertyCollection.java
@@ -1,2 +1,59 @@
-package com.company.abstractClasses.properties;public class PropertyCollection {
+package com.company.abstractClasses.properties;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.company.abstractClasses.properties.Type.TERRACED;
+
+public class PropertyCollection {
+    private final Set<Property> portfolio;
+
+    public PropertyCollection(){
+        this.portfolio = new HashSet<Property>();
+    }
+    // Add a property to the collection
+    public void addProperty(Property p){
+        this.portfolio.add(p);
+    }
+
+    // Return the set of all houses in the collection
+    public Set<House> getHouses() {
+        return this.portfolio.stream().
+                filter(item -> item instanceof House)
+                .map(item -> (House) item)
+                .collect(Collectors.toSet());
+    }
+
+    // Return the set of all bungalows in the collection
+    public Set<Bungalow> getBungalows() {
+        return this.portfolio.stream().
+                filter(item -> item instanceof Bungalow)
+                .map(item -> (Bungalow) item)
+                .collect(Collectors.toSet());
+    }
+
+    // Return the set of all flats in the collection
+    public Set<Flat> getFlats() {
+        return this.portfolio.stream().
+                filter(item -> item instanceof Flat)
+                .map(item -> (Flat) item)
+                .collect(Collectors.toSet());
+    }
+
+    // Return the set of all maisonettes in the collection
+    public Set<Maisonette> getMaisonettes(){
+        return this.portfolio.stream().
+                filter(item -> item instanceof Maisonette)
+                .map(item -> (Maisonette) item)
+                .collect(Collectors.toSet());
+    }
+
+    // Return the set of all terraced houses in the collection
+    public Set<House> getTerracedHouses(){
+        return this.getHouses()
+                .stream()
+                .filter(item -> item.getHouseType() == TERRACED)
+                .collect(Collectors.toSet());
+    }
 }
